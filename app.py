e7534b9ebc9c84f1d00b2421570e5dd4b58df79a
Srivastava0109Yash <yashsri0109@gmail.com>
2023-09-05 15:04:34 +0530

    CFO GPT

--- app.py
+++ app.py
@@ -1,67 +1,127 @@
 import chainlit as cl
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.chroma import Chroma
-from langchain.embeddings import OpenAIEmbeddings
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings,HuggingFaceInstructEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import UnstructuredHTMLLoader, PyPDFLoader, CSVLoader
-import os
+from langchain.document_loaders import WebBaseLoader,UnstructuredURLLoader, PyPDFLoader, CSVLoader,JSONLoader,UnstructuredMarkdownLoader
+import os,pickle
+from langchain import OpenAI
+from bs4 import BeautifulSoup
+import json,markdown,csv,io,textract
+from pathlib import Path
+from openpyxl import load_workbook
 
 welcome_message = """### Welcome to Chat with 👋
 You can upload a file or paste a url to chat with.
 I support many types of files and urls.
 """
 
-supported_file_types = [
-    "text/plain",  # Text
-    "text/html",  # HTML
-    "application/pdf",  # PDF
-    "text/csv",  # CSV
-]
 
+supported_file_types = ["pdf","html","plain","json","markdown","python-script"]
 
 embeddings = OpenAIEmbeddings()
 memory = ConversationBufferMemory(
     memory_key="chat_history", return_messages=True
 )
-llm = ChatOpenAI(temperature=0, model="gpt-4")
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
 condense_question_llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200)
+    chunk_size=2500, chunk_overlap=200)
 
+def get_files_text(file_response):
+    if file_response is None:
+        return ""
 
-def create_vectorstore(file):
+    text = ""
 
-    with open(file.name, 'wb') as _file:
-        _file.write(file.content)
+    # Access the content and name properties directly
+    file_content = file_response.content
+    file_name = file_response.name
 
-    file_path = os.path.abspath(file.name)
+    if file_content is None:
+        return ""
 
-    if file.type == "text/plain":
-        text = file.content.decode("utf-8")
-        texts = text_splitter.split_text(text)
-        vectorstore = Chroma.from_texts(texts, embeddings)
-        return vectorstore
+    if file_name.endswith('.pdf'):
+        pdf_reader = PdfReader(io.BytesIO(file_content))
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    elif file_name.endswith('.txt'):
+        text += file_content.decode('utf-8')
+    elif file_name.endswith('.md'):
+        md_content = file_content.decode('utf-8')
+        html_content = markdown.markdown(md_content)
+        soup = BeautifulSoup(html_content, 'html.parser')
+        text += soup.get_text()
+    elif file_name.endswith('.json'):
+        json_content = json.loads(file_content)
+        if isinstance(json_content, dict):
+            text += ' '.join(str(value) for value in json_content.values())
+        elif isinstance(json_content, list):
+            text += ' '.join(str(item) for item in json_content)
+    elif file_name.endswith('.csv'):
+        csv_content = file_content.decode('utf-8')
+        csv_file = io.StringIO(csv_content)
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            text += ' '.join(row) + ' '
+    elif file_name.endswith('.xlsx'):
+        workbook = load_workbook(io.BytesIO(file_content), read_only=True)
+        for sheet in workbook:
+            for row in sheet.iter_rows():
+                for cell in row:
+                    text += str(cell.value) + ' '
+    elif file_name.endswith('.py'):
+        text += file_content.decode('utf-8')
 
-    if file.type == "text/html":
-        loader = UnstructuredHTMLLoader(file_path)
+    return text
 
-    if file.type == "application/pdf":
-        loader = PyPDFLoader(file_path)
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=2000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+def get_text_chunks1(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=500,
+        chunk_overlap=200,
+    )
+    chunks = text_splitter.split_documents(text)
+    return chunks
+    
 
-    if file.type == "text/csv":
-        loader = CSVLoader(file_path)
-
-    documents = loader.load()
-    documents = text_splitter.split_documents(documents)
-    vectorstore = Chroma.from_documents(documents, embeddings)
-
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
+
+def get_vectorstore1(text_chunks):
+     embeddings = OpenAIEmbeddings()
+     #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+     vectorstore = FAISS.from_documents(documents=text_chunks, embedding=embeddings)
+     return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
 
 
 @cl.langchain_factory
@@ -70,6 +130,7 @@ def main():
 
     cleaned_resp = None
     valid_responses = ["file", "url"]
+    
     while cleaned_resp not in valid_responses:
         resp = cl.AskUserMessage(
             "What would you like to chat with, a File or a URL?"
@@ -77,27 +138,22 @@ def main():
         if 'content' in resp:
             cleaned_resp = resp['content'].strip().lower()
 
-            if cleaned_resp == "url":
-                cl.Message(
-                    content="URLs are not supported yet. Please upload a file."
-                ).send()
-                cleaned_resp = "file"
-
     if cleaned_resp == "file":
         file = None
         while file is None:
             file = cl.AskFileMessage(
-                content='Upload your file:', accept=supported_file_types, timeout=180
+                content='Upload your file:', accept=supported_file_types, timeout=180, max_size_mb=500,
             ).send()
 
-        vectorstore = create_vectorstore(file)
+        raw_text = get_files_text(file)
 
-        chain = ConversationalRetrievalChain.from_llm(
-            llm,
-            vectorstore.as_retriever(),
-            condense_question_llm=condense_question_llm,
-            memory=memory,
-        )
+        # get the text chunks
+        text_chunks = get_text_chunks(raw_text)
+
+        # create vector store
+        vectorstore = get_vectorstore(text_chunks)
+
+        chain = get_conversation_chain(vectorstore)
 
         cl.Message(
             content=f"`{file.name}` uploaded! How can I help you?"
@@ -105,5 +161,26 @@ def main():
 
         return chain
 
-    # if cleaned_resp == "url":
-    #     pass
+    if cleaned_resp == "url":
+        url = None
+        while url is None:
+            url = cl.AskUserMessage(
+                content="Paste Your URL:"
+            ).send()
+            url = url['content']
+        
+        print(type(url))
+        
+        loaders=WebBaseLoader(url)
+
+        data = loaders.load()
+        print(data)
+        docs = get_text_chunks1(data)
+        vectorstore = get_vectorstore1(docs)
+        chain = get_conversation_chain(vectorstore)
+
+        cl.Message(
+            content=f"URL uploaded! How can I help you?"
+        ).send()
+
+        return chain
